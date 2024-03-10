73f81222619c185698f7747e836f9c38d7bb7a43
gaby <gabsy3@gmail.com>
2024-03-10 11:29:58 +0200

    add header and router outlet

--- testresultstrainees/src/app/components/header/header.component.ts
+++ testresultstrainees/src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink , RouterLinkActive],
+  imports: [RouterLink , RouterLinkActive , MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
