6077f46a8af761d538883642747b582f6fa80dd3
gaby <gabsy3@gmail.com>
2024-03-10 11:16:18 +0200

    add routes components and folders

--- testresultstrainees/src/app/app.routes.ts
+++ testresultstrainees/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DataComponent } from './components/data/data.component';
+import { AnalysisComponent } from './components/analysis/analysis.component';
+import { MonitorComponent } from './components/monitor/monitor.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DataComponent,
+  },
+  { path: 'data', component: DataComponent },
+  { path: 'analysis', component: AnalysisComponent },
+  { path: 'monitor', component: MonitorComponent },
+];
