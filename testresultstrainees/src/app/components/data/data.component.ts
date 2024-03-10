b7f9fc2eaeb8017f0b37440265835f7e7f4044b1
gaby <gabsy3@gmail.com>
2024-03-10 14:09:00 +0200

    data component

--- testresultstrainees/src/app/components/data/data.component.ts
+++ testresultstrainees/src/app/components/data/data.component.ts
@@ -1,10 +1,14 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule, MatTableDataSource } from '@angular/material/table';
-import { dataGridModel, trainee } from '../../models/data.model';
+import {
+  MatTableModule,
+  MatTableDataSource,
+  MatTable,
+} from '@angular/material/table';
+import { trainee } from '../../models/data.model';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+
 import {
   FormControl,
   FormGroup,
@@ -13,23 +17,77 @@ import {
   Validators,
 } from '@angular/forms';
 
-const ELEMENT_DATA: dataGridModel[] = [
-  { id: '1', name: 'Gabi', date: '01/03/2024', grade: 80, subject: 'Algebra' },
-  { id: '2', name: 'Maly', date: '08/03/2024', grade: 85, subject: 'History' },
-  { id: '3', name: 'Yossi', date: '10/02/2024', grade: 100, subject: 'test' },
+const ELEMENT_DATA: trainee[] = [
+  {
+    id: '1',
+    name: 'Gabi',
+    date_joined: '01/03/2024',
+    grade: 80,
+    subject: 'Algebra',
+  },
+  {
+    id: '2',
+    name: 'Maly',
+    date_joined: '08/03/2024',
+    grade: 85,
+    subject: 'History',
+  },
+  {
+    id: '3',
+    name: 'Yossi',
+    date_joined: '10/02/2024',
+    grade: 100,
+    subject: 'test',
+  },
   {
     id: '4',
     name: 'Beryllium',
-    date: '10/03/2024',
+    date_joined: '10/03/2024',
     grade: 30,
     subject: 'test',
   },
-  { id: '5', name: 'Boron', date: '12/05/2023', grade: 65, subject: 'test' },
-  { id: '6', name: 'Carbon', date: '10/03/2023', grade: 87, subject: 'test' },
-  { id: '7', name: 'Nitrogen', date: '29/01/2024', grade: 77, subject: 'test' },
-  { id: '8', name: 'Oxygen', date: '10/03/2024', grade: 90, subject: 'test' },
-  { id: '9', name: 'Fluorine', date: '10/03/2024', grade: 55, subject: 'test' },
-  { id: '10', name: 'Neon', date: '10/03/2024', grade: 80, subject: 'test' },
+  {
+    id: '5',
+    name: 'Boron',
+    date_joined: '12/05/2023',
+    grade: 65,
+    subject: 'test',
+  },
+  {
+    id: '6',
+    name: 'Carbon',
+    date_joined: '10/03/2023',
+    grade: 87,
+    subject: 'test',
+  },
+  {
+    id: '7',
+    name: 'Nitrogen',
+    date_joined: '29/01/2024',
+    grade: 77,
+    subject: 'test',
+  },
+  {
+    id: '8',
+    name: 'Oxygen',
+    date_joined: '10/03/2024',
+    grade: 90,
+    subject: 'test',
+  },
+  {
+    id: '9',
+    name: 'Fluorine',
+    date_joined: '10/03/2024',
+    grade: 55,
+    subject: 'test',
+  },
+  {
+    id: '10',
+    name: 'Neon',
+    date_joined: '10/03/2024',
+    grade: 80,
+    subject: 'test',
+  },
 ];
 
 @Component({
@@ -46,24 +104,29 @@ const ELEMENT_DATA: dataGridModel[] = [
   styleUrl: './data.component.scss',
 })
 export class DataComponent implements AfterViewInit {
-  displayedColumns: string[] = ['id', 'name', 'date', 'grade', 'subject'];
-  dataSource = new MatTableDataSource<dataGridModel>(ELEMENT_DATA);
+  displayedColumns: string[] = ['id', 'name', 'date_joined', 'grade', 'subject'];
+  dataSource = new MatTableDataSource<trainee>(ELEMENT_DATA);
+  dataSource2 = [...ELEMENT_DATA]
   showDetails: boolean = false;
   traineeForm = new FormGroup({
     id: new FormControl('', Validators.required),
     name: new FormControl('', Validators.required),
     grade: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
+    email: new FormControl(''),
     date_joined: new FormControl('', Validators.required),
-    address: new FormControl('', Validators.required),
-    city: new FormControl('', Validators.required),
-    country: new FormControl('', Validators.required),
-    zip: new FormControl('', Validators.required),
+    address: new FormControl(''),
+    city: new FormControl(''),
+    country: new FormControl(''),
+    zip: new FormControl(''),
     subject: new FormControl('', Validators.required),
   });
 
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
+  gridChecked: boolean = false;
+
+  @ViewChild(MatTable)
+  table!: MatTable<trainee>;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -75,8 +138,16 @@ export class DataComponent implements AfterViewInit {
     this.showDetails = false;
   }
   saveTrainee() {
-    if(this.traineeForm.status === 'VALID'){
-      
+    if (this.traineeForm.status === 'VALID') {
+      const { id, name, grade, date_joined, subject } = this.traineeForm.value;
+      if(id && name &&  grade && date_joined &&  subject){
+        this.dataSource2.push({ id, name, grade: +grade, date_joined, subject });
+        this.table.renderRows();
+      }
     }
+  }
+  removeTrainee() {
+    // this.dataSource.pop();
+    // this.table.renderRows();
   }
 }
--- testresultstrainees/src/app/models/data.model.ts
+++ testresultstrainees/src/app/models/data.model.ts
@@ -1,20 +1,12 @@
-export interface dataGridModel {
-  id: string;
-  name: string;
-  date: string;
-  grade: number;
-  subject: string;
-}
-
 export interface trainee {
   id: string;
   name: string;
   grade: number;
-  email:string;
+  email?:string;
   date_joined: string;
-  address:string;
-  city:string;
-  country:string;
-  zip:string;
+  address?:string;
+  city?:string;
+  country?:string;
+  zip?:string;
   subject: string;
 }
