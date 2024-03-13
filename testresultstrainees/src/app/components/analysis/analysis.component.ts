57aa38ab3a83227dfc5f9ff53eb0c2f0b5b96eb6
gaby <gabsy3@gmail.com>
2024-03-13 19:50:57 +0200

    analysis  component

--- testresultstrainees/src/app/components/analysis/analysis.component.ts
+++ testresultstrainees/src/app/components/analysis/analysis.component.ts
@@ -7,6 +7,10 @@ import { MatSelectModule } from '@angular/material/select';
 import { TraineeService } from '../../services/trainee.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { trainee } from '../../models/data.model';
+import { ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+
 
 @Component({
   selector: 'app-analysis',
@@ -18,37 +22,54 @@ import { trainee } from '../../models/data.model';
     ReactiveFormsModule,
     MatCheckboxModule,
     MatInputModule,
+    BaseChartDirective
   ],
   templateUrl: './analysis.component.html',
   styleUrl: './analysis.component.scss',
 })
 export class AnalysisComponent implements OnInit {
+
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: false,
+  };
+  public pieChartLabels = [ [ 'Download' ], [ 'Sales' ], 'Mail' ];
+  public pieChartDatasets = [ {
+    data: [ 300, 500, 100 ]
+  } ];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
   traineeService = inject(TraineeService);
   IDs = new FormControl('');
   subjects = new FormControl('');
   IDsListDuplicate: any = [];
-  subjectListDuplicates : any = [];
+  subjectListDuplicates: any = [];
   idsList: any = [];
-  subjectList:any = [];
+  subjectList: any = [];
   selectedIds = [];
   selectedSubject = [];
   dataSource = new MatTableDataSource<trainee>(
     this.traineeService.ELEMENT_DATA
   );
   ngOnInit(): void {
+    this.configChart();
     this.IDsListDuplicate = this.traineeService.ELEMENT_DATA.map(
       (data) => data.studentId
     );
     this.idsList = this.IDsListDuplicate.filter(
       (item: any, index: any) => this.IDsListDuplicate.indexOf(item) === index
     );
-    this.subjectListDuplicates = this.dataSource.data.map(item=> item.subject);
-    console.log(this.subjectListDuplicates);
-    this.subjectList = this.subjectListDuplicates.filter(
-      (item: any, index: any) => this.subjectListDuplicates.indexOf(item) === index
+    this.subjectListDuplicates = this.dataSource.data.map(
+      (item) => item.subject
     );
-    
+    this.subjectList = this.subjectListDuplicates.filter(
+      (item: any, index: any) =>
+        this.subjectListDuplicates.indexOf(item) === index
+    );
   }
   displayIdChart() {}
   displaySubjectChart() {}
+  configChart() {
+    
+  }
 }
