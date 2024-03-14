e4b3ea845cc19b56e00d611e8e0060206283e79d
gaby <gabsy3@gmail.com>
2024-03-14 13:03:25 +0200

    analysis  component

--- testresultstrainees/src/app/components/analysis/analysis.component.ts
+++ testresultstrainees/src/app/components/analysis/analysis.component.ts
@@ -10,8 +10,6 @@ import { trainee } from '../../models/data.model';
 import { ChartOptions } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
-
-
 @Component({
   selector: 'app-analysis',
   standalone: true,
@@ -22,22 +20,51 @@ import { BaseChartDirective } from 'ng2-charts';
     ReactiveFormsModule,
     MatCheckboxModule,
     MatInputModule,
-    BaseChartDirective
+    BaseChartDirective,
   ],
   templateUrl: './analysis.component.html',
   styleUrl: './analysis.component.scss',
 })
 export class AnalysisComponent implements OnInit {
-
-  public pieChartOptions: ChartOptions<'pie'> = {
+  /*chart 1*/
+  public pieChartOptions1: ChartOptions<'pie'> = {
     responsive: false,
   };
-  public pieChartLabels :any = [];
-  public pieChartDatasets:any = [ {
-    data: [70,30]
-  } ];
-  public pieChartLegend = true;
-  public pieChartPlugins = [];
+  public pieChartLabels1: any = [];
+  public pieChartDatasets1: any = [
+    {
+      data: [],
+    },
+  ];
+  public pieChartLegend1 = true;
+  public pieChartPlugins1 = [];
+
+  /*chart 2*/
+  public pieChartOptions2: ChartOptions<'pie'> = {
+    responsive: false,
+  };
+  public pieChartLabels2: any = [];
+  public pieChartDatasets2: any = [
+    {
+      data: [],
+    },
+  ];
+  public pieChartLegend2 = true;
+  public pieChartPlugins2 = [];
+  sumAvgAllStudent = 0;
+
+  /*chart 3*/
+  public pieChartOptions3: ChartOptions<'pie'> = {
+    responsive: false,
+  };
+  public pieChartLabels3: any = [];
+  public pieChartDatasets3: any = [
+    {
+      data: [],
+    },
+  ];
+  public pieChartLegend3 = true;
+  public pieChartPlugins3 = [];
 
   traineeService = inject(TraineeService);
   IDs = new FormControl('');
@@ -46,7 +73,7 @@ export class AnalysisComponent implements OnInit {
   subjectListDuplicates: any = [];
   idsList: any = [];
   subjectList: any = [];
-  selectedIds :any  = [];
+  selectedIds: any = [];
   selectedSubject = [];
   dataSource = new MatTableDataSource<trainee>(
     this.traineeService.ELEMENT_DATA
@@ -69,22 +96,42 @@ export class AnalysisComponent implements OnInit {
   }
   displayIdChart() {
     let studentMarks = this.ds.filter((item: any) =>
+      this.selectedIds?.includes(item.studentId)
+    );
+    let DuplicateSnames = studentMarks.map((item) => item.name);
+    let names = DuplicateSnames.filter(
+      (item, index) => DuplicateSnames.indexOf(item) === index
+    );
+    this.pieChartLabels1 = names;
+
+    let std: any = studentMarks.filter(
+      (item, index) => names[index] === item.name
+    );
+    let avgPerStd = std.map((item: any) => item.average);
+    this.pieChartDatasets1[0].data = avgPerStd;
+    //this.pieChartDatasets[0].backgroundColor = "rgb(54, 162, 235)";
+  }
+  displayIdChart2() {
+    this.sumAvgAllStudent = 0;
+    let studentMarks = this.ds.filter((item: any) =>
         this.selectedIds?.includes(item.studentId)
     );
-    console.log("studentsMarks",studentMarks);
     let DuplicateSnames = studentMarks.map(item=>item.name);
     let names = DuplicateSnames.filter(
       (item, index) => DuplicateSnames.indexOf(item) === index
     );
-    this.pieChartLabels = names;
+    this.pieChartLabels2 = names;
+    console.log(this.pieChartLabels2);
 
-    
     let std:any = studentMarks.filter(
       (item, index) => names[index] === item.name
     );
-    console.log(std);
     let avgPerStd = std.map((item:any)=> item.average)
-    this.pieChartDatasets[0].data = avgPerStd;
+    avgPerStd.forEach((element:any) => {
+      this.sumAvgAllStudent += element;
+    });
+    const avg = this.sumAvgAllStudent / avgPerStd.length;
+    this.pieChartDatasets2[0].data = avg;
   }
   displaySubjectChart() {}
 }
